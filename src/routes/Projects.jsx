5da56babd509dd673c4d703ebb330940a9d5a500
Cajsa Bagge <cajsabagge@hotmail.com>
2025-08-25 16:37:23 +0200

    Building project page, but had to do a commit to secure a nice waiting page

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -3,35 +3,16 @@
 export default function Projects() {
   return (
     <section className="py-16 px-4">
-      <div className="max-w-4xl mx-auto">
-        <h2 className="text-3xl font-bold text-center mb-10">Mina Projekt</h2>
-        <div className="grid gap-6 sm:grid-cols-2">
-          {/* React-projektet */}
-          <div className="bg-purple-800 p-6 rounded-xl shadow hover:shadow-lg transition">
-            <h3 className="text-xl font-semibold mb-2">React-projektet</h3>
-            <p className="mb-4">
-              Byggde en webbapp i React med komponenter, props och state.
-              Fokus låg på användarvänlig design och responsiv layout.
-            </p>
-            <a
-              href="https://github.com/CajsaBagge/projektarbete"
-              className="text-white hover:underline"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Se koden på GitHub →
-            </a>
-          </div>
-          
-          <div className="bg-purple-800 p-6 rounded-xl shadow hover:shadow-lg transition">
-            <h3 className="text-xl font-semibold mb-2">HTML/CSS-projekt</h3>
-            <p className="mb-4">Ett projekt där jag jobbade med strukturerad layout och modern design utifrån en wireframe.</p>
-            <a href="https://github.com/CajsaBagge/HTMLochCSSprojekt.git" className=" hover:underline" target="_blank" rel="noopener noreferrer">
-              Se koden på GitHub →
-            </a>
-          </div>
-        </div>
-      </div>
+     <div>
+      <h1 className="text-5xl sm:text-6xl md:text-8xl font-bold text-center text-orange-600">Projekt</h1>
+      <p className="text-lg leading-relaxed tracking-wider text-center mt-10">
+        Min portfoliohemsida är just nu i byggläge (tänk “work in progress meets kaos och kaffe”)</p>
+        <img
+            src="buildpic.png"
+            alt="About me"
+            className="max-w-96 mx-auto mt-10"
+          />
+     </div>
     </section>
   )
 }
